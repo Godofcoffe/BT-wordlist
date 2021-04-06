dd7d59835970d42e3e528f2a6553558c3266ee95
Kevin G. Tertuliano <robotnyte@gmail.com>
2021-04-06 12:43:22 -0300

    :sparkles: stylized text

--- BD/teste_arquivo.py
+++ BD/teste_arquivo.py
@@ -1,9 +1,10 @@
 from interface.menu import *
+from interface.form_text import *
 
 
 def arquivo_existe(nome):
     """
-Testa se o arquivo existe,apenas argumente o nome do arquivo.
+Tests if the file exists, just argue the file name.
     """
     try:
         open(nome, 'rt')
@@ -15,22 +16,22 @@ Testa se o arquivo existe,apenas argumente o nome do arquivo.
 
 def criar_arqv(nome):
     """
-Cria um novo arquivo,apenas argumente o nome do arquivo.
+Create a new file, just argue the file name.
     """
     try:
         open(nome, 'wt+')
     except:
-        print('Error creating the file')
+        print(color_text('red', 'Error creating the file'))
 
 
 def ler_arqv(nome):
     """
-Imprime o arquivo txt,apenas argumente o nome do arquivo.
+Print the txt file, just argue the file name.
     """
     try:
         open(nome, 'rt')
     except FileExistsError or PermissionError:
-        print('ERROR reading file')
+        print(color_text('red', 'ERROR reading file'))
     else:
         with open(nome, 'rt') as arqv:
             cabeçalho('SAVED DATA.')
@@ -40,15 +41,15 @@ Imprime o arquivo txt,apenas argumente o nome do arquivo.
 
 def add_w(arq, arg1='vazio'):
     """
-Aqui serão adicionados as tentativas para a wordlist.
+Attempts for the wordlist will be added here.
     """
     try:
         open(arq, 'at')
     except FileExistsError or PermissionError:
-        print('Error opening file.')
+        print(color_text('red', 'Error opening file.'))
     else:
         try:
             with open(arq, 'at') as arqv:
                 arqv.write(f'{arg1}\n')
         except FileExistsError or PermissionError:
-            print('Error adding to WordList.')
+            print(color_text('red', 'Error adding to WordList.'))
